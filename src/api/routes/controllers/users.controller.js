dbe0c280d2f7ded3df85c4606d9b081106278528
Chan <lhc0506@gmail.com>
2022-02-08 17:21:11 +0900

    refact

--- src/api/routes/controllers/users.controller.js
+++ src/api/routes/controllers/users.controller.js
@@ -2,7 +2,7 @@ const usersService = require("../../services/users.service");
 
 exports.create = async (req, res, next) => {
   try {
-    const result = await usersService.signup(req.body.user);
+    await usersService.signup(req.body.user);
 
     res.json({
       result: "ok",
@@ -13,12 +13,12 @@ exports.create = async (req, res, next) => {
 };
 
 exports.getGoals = async (req, res, next) => {
-  const { createdGoals } = req.app.locals.authResult;
-  const result = await usersService.getGoalsFromIds(createdGoals);
+  try {
+    const { createdGoals } = req.app.locals.authResult;
+    const result = await usersService.getGoalsFromIds(createdGoals);
 
-  if (result?.error) {
-    return next(result.error);
+    res.json({ result });
+  } catch (error) {
+    next(error);
   }
-
-  res.json({ result });
 };
