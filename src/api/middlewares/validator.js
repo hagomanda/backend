82f536c8efe6174040f52d6f957b2e7308f8fc7c
soozynn <81217456+soozynn@users.noreply.github.com>
2022-02-16 11:18:52 +0900

    Update src/api/middlewares/validator.js

--- src/api/middlewares/validator.js
+++ src/api/middlewares/validator.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 const Joi = require("joi");
 const jwt = require("jsonwebtoken");
-const { string } = require("joi");
 
 exports.verifyParams = (req, res, next) => {
   const { id } = req.params;
